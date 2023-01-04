e85b32d1126aa9bfdbea205c38f1442c8c071086
Pascal Rainville <pascal.rainville@gmail.com>
2023-01-04 09:45:40 -0500

    .

---
+++ app/src/main/java/com/heda/Recipe.kt
@@ -0,0 +1,5 @@
+package com.heda
+
+data class Recipe (
+    var title: String
+)
---
+++ app/src/main/java/com/heda/RecipeAdapter.kt
@@ -0,0 +1,41 @@
+package com.heda
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recipe_item.view.*
+
+class RecipeAdapter (
+    private val recipes: MutableList<Recipe>
+) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>()  {
+
+    class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        return RecipeViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.recipe_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    fun addTodo(todo: Recipe) {
+        recipes.add(todo)
+        notifyItemInserted(recipes.size - 1)
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        val curTodo = recipes[position]
+        holder.itemView.apply {
+            tvRecipeTitle.text = curTodo.title
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return recipes.size
+    }
+
+}
