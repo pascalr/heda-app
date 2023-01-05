cd626a7a4080a60e3382ef356aec5cad12fba270
Pascal Rainville <pascal.rainville@gmail.com>
2023-01-05 09:55:24 -0500

    .

--- app/src/main/java/com/heda/StartActivity.kt
+++ app/src/main/java/com/heda/StartActivity.kt
@@ -4,15 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.heda.ui.login.LoginActivity
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-        Toast.makeText(applicationContext, "StartActivity onCreate", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(applicationContext, "StartActivity onCreate", Toast.LENGTH_SHORT).show()
 
-        val i = Intent(this@StartActivity, MainActivity::class.java)
-        startActivity(i)
+        if (true) {
+            val i = Intent(this@StartActivity, MainActivity::class.java)
+            startActivity(i)
+        } else {
+            val i = Intent(this@StartActivity, LoginActivity::class.java)
+            startActivity(i)
+        }
     }
 }
