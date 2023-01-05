d40eb7b3a6f8075aeb0f6d42ce56446d34450770
Pascal Rainville <pascal.rainville@gmail.com>
2023-01-05 14:30:12 -0500

    .

--- app/src/main/java/com/heda/StartActivity.kt
+++ app/src/main/java/com/heda/StartActivity.kt
@@ -1,8 +1,85 @@
 package com.heda
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
+data class FetchDataUser(
+    val name: String
+)
+
+data class FetchDataRecipe(
+    val name: String,
+    val id: Int,
+    val user_id: Int,
+    val recipe_kind_id: Int,
+    val preparation_time: Int,
+    val cooking_time: Int,
+    val total_time: Int,
+    val json: String,
+    val ingredients: String,
+    val image_slug: String,
+    val original_id: Int,
+    val is_public: Int,
+    val raw_servings: String,
+    val heda_instructions: String
+)
+
+data class FetchData(
+    val user: FetchDataUser,
+    val userRecipes: List<FetchDataRecipe>,
+    val favRecipes: List<FetchDataRecipe>
+)
+
+fun getData(callback: (String) -> Any?) {
+    val client = OkHttpClient()
+    val request = Request.Builder()
+        .url("https://www.hedacuisine.com/fetch_user/5")
+        .build()
+
+    client.newCall(request).execute().use { response ->
+        if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+        for ((name, value) in response.headers) {
+            println("$name: $value")
+        }
+
+        val data = response.body!!.string()
+        callback(data)
+    }
+}
+
+/**
+ * A view model persists based on it's scope.
+ */
+class FetchDataViewModel: ViewModel() {
+
+    init {
+        val thread = Thread {
+            try {
+                getData { data ->
+                    println(data)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        thread.start()
+        // FIXME: I don't understand why this does not work...
+        //viewModelScope.launch {
+        //    getData { data ->
+        //        println(data)
+        //    }
+        //}
+    }
+}
 
 class StartActivity : AppCompatActivity() {
 
@@ -25,6 +102,8 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Thread.sleep(10000)
+
         //Toast.makeText(applicationContext, "StartActivity onCreate", Toast.LENGTH_SHORT).show()
 
         // How to share data between activities: Try:
@@ -32,6 +111,8 @@ class StartActivity : AppCompatActivity() {
 
         // Maybe use: onSaveInstanceState and onRestoreInstanceState like here:
         // https://www.tutorialspoint.com/how-to-save-an-android-activity-state-using-save-instance-state
+
+        val fetchDataViewModel = ViewModelProvider(this)[FetchDataViewModel::class.java]
 
         val loggedIn = true
 
--- app/src/main/java/com/heda/SyncingActivity.kt
+++ app/src/main/java/com/heda/SyncingActivity.kt
@@ -6,52 +6,6 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.IOException
 
-data class FetchDataUser(
-    val name: String
-)
-
-data class FetchDataRecipe(
-    val name: String,
-    val id: Int,
-    val user_id: Int,
-    val recipe_kind_id: Int,
-    val preparation_time: Int,
-    val cooking_time: Int,
-    val total_time: Int,
-    val json: String,
-    val ingredients: String,
-    val image_slug: String,
-    val original_id: Int,
-    val is_public: Int,
-    val raw_servings: String,
-    val heda_instructions: String
-)
-
-data class FetchData(
-    val user: FetchDataUser,
-    val userRecipes: List<FetchDataRecipe>,
-    val favRecipes: List<FetchDataRecipe>
-)
-
-fun getData() {
-    Thread.sleep(10000)
-
-    val client = OkHttpClient()
-    val request = Request.Builder()
-        .url("https://www.hedacuisine.com/fetch_user/5")
-        .build()
-
-    client.newCall(request).execute().use { response ->
-        if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-        for ((name, value) in response.headers) {
-            println("$name: $value")
-        }
-
-        println(response.body!!.string())
-    }
-}
-
 class SyncingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
