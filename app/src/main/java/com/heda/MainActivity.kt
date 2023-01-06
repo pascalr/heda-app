24080fe125cafd5586743109dc47ed01eba0d8ea
Pascal Rainville <pascal.rainville@gmail.com>
2023-01-06 13:06:04 -0500

    .

--- app/src/main/java/com/heda/MainActivity.kt
+++ app/src/main/java/com/heda/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.heda
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import com.heda.view_models.RouterViewModel
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
